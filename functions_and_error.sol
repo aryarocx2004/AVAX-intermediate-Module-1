ef04a85fbbbb87c454cc7d25982c527b56aa0c3a
aryarocx2004 <144051136+aryarocx2004@users.noreply.github.com>
2023-11-12 19:51:06 +0530

    Update functions_and_error.sol

--- functions_and_error.sol
+++ functions_and_error.sol
@@ -1,4 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ErrorHandlingContract {
+    address public owner;
+    uint256 public value;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setValue(uint256 _newValue) external {
+        // Using require() for input validation
+        require(msg.sender == owner, "Only the owner can set the value");
+        
+        // Using assert() for internal error checking
+        assert(_newValue > 0);
+
+        // Using revert() with a custom error message
+        if (_newValue == 42) {
+            revert("The value cannot be 42");
+        }
+
+        // Set the new value
+        value = _newValue;
+    }
+}
 
 
 
