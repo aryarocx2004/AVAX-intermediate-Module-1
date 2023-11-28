8a3549d3568888cda3cb3be06b5d044450fa9f15
aryarocx2004 <144051136+aryarocx2004@users.noreply.github.com>
2023-11-28 16:50:47 +0530

    Update functions_and_error.sol

--- functions_and_error.sol
+++ functions_and_error.sol
@@ -1,30 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ErrorHandlingContract {
-    address public owner;
-    uint256 public value;
+contract DriversLicenseEligibility {
 
-    constructor() {
-        owner = msg.sender;
-    }
+    function checkEligibility(uint256 age) external pure returns (string memory) {
+        // Use assert to check an assertion
+        assert(age >= 0);
 
-    function setValue(uint256 _newValue) external {
-        // Using require() for input validation
-        require(msg.sender == owner, "Only the owner can set the value");
-        
-        // Using assert() for internal error checking
-        assert(_newValue > 0);
+        // Use require to check a condition
+        require(age >= 16, "Minimum age for a driver's license is 16 years");
 
-        // Using revert() with a custom error message
-        if (_newValue == 42) {
-            revert("The value cannot be 42");
+        // Eligibility criteria
+        if (age >= 18) {
+            return "Eligible for a driver's license";
+        } else {
+            // Use revert to revert the transaction with a custom message
+            revert("Not eligible for a driver's license yet");
         }
-
-        // Set the new value
-        value = _newValue;
     }
 }
-
-
-
